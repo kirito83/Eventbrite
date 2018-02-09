b5332e666d63d2c2861db43a23f0f55283df3fc3
Triqueneaux Calvin <calvin8313@hotmail.fr>
2018-02-09 14:45:30 +0100

    pimp fiolaxie

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-  	base_title = "Eventbrite"
+  	base_title = "Fiolaxie"
   	if page_title.empty?
   		base_title
   	else
